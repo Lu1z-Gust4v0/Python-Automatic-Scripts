2afa0d35512d3ca2fdc6ab8b70a2f4233a3ba2ad
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2024-05-06 23:50:39 -0300

    feat: add robot that sends emails

--- tasks/get_started.py
+++ tasks/get_started.py
@@ -2,12 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Chrome()
+options = webdriver.ChromeOptions()
+options.add_argument("--headless=new")
+browser = webdriver.Chrome(options=options)
+
+print("Browsed initialized")
 
 browser.get("http://www.yahoo.com")
 assert "Yahoo" in browser.title
+
+print("Page accessed successfully")
 
 elem = browser.find_element(By.NAME, "p")  # Find the search box
 elem.send_keys("seleniumhq" + Keys.RETURN)
 
 browser.quit()
+print("Quitting...")
