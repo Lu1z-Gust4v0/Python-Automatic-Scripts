9fc38d4f45ae37726a34d1036367020f0cc3c326
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2024-05-09 23:47:29 -0300

    feat: add save page function

--- tasks/utils.py
+++ tasks/utils.py
@@ -1,4 +1,9 @@
+import math
+import random
+
+from PIL import Image
 from selenium import webdriver
+from selenium.webdriver import Chrome
 from selenium.webdriver.support.ui import WebDriverWait
 
 TIMEOUT_IN_SECONDS = 30
@@ -14,3 +19,16 @@ def init_web_driver(arguments: list[str]):
     waiter = WebDriverWait(browser, TIMEOUT_IN_SECONDS)
 
     return browser, waiter
+
+
+def save_page(browser: Chrome, open_screenshot=False):
+    page_id = math.floor(random.random() * 100)
+
+    browser.save_screenshot(f"saved_page_{page_id}.png")
+
+    if open_screenshot:
+        screenshot = Image(f"saved_page_{page_id}.png")
+        screenshot.show()
+
+    with open(f"saved_page_{page_id}.html", "w", encoding="utf-8") as file:
+        file.write(browser.page_source)
